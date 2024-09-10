6afe918f3e069fd78316772fcadd3ec4fe3545fe
bintangdeveloper <bintangdeveloper@gmail.com>
2024-09-10 09:18:11 +0700

    +

--- resources/views/auth/signIn.blade.php
+++ resources/views/auth/signIn.blade.php
@@ -5,11 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="referrer" content="always">
-        <link rel="canonical" href="{{ $page->getUrl() }}">
+        --{{
+        <link rel="canonical" href="{{ $page->getUrl() ?? }}">
+        }}--
 
-        <meta name="description" content="{{ $page->description }}">
+        <meta name="description" content="Login to the dashboard">
 
-        <title>{{ $page->title }}</title>
+        <title>Login</title>
         
         @production
           @vite('resources/css/app.css')
