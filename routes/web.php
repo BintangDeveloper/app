9bba389d0c503a7751841e12139044826ee5daa5
bintangdeveloper <bintangdeveloper@gmail.com>
2024-09-15 07:35:42 +0700

    +

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,35 @@ Route::get('/welcome', function () {
 });
 
 Route::group(['middleware' => 'guest'], function () {
-  Route::get('/auth/register', [AuthController::class, 'register'])->name('register');
-  Route::post('/auth/register', [AuthController::class, 'registerPost'])->name('register');
-  Route::get('/auth/login', [AuthController::class, 'login'])->name('login');
-  Route::post('/auth/login', [AuthController::class, 'loginPost'])->name('login');
+  Route::get('/auth/register', [
+    AuthController::class, 'register'
+  ])->name('register');
+  Route::post('/auth/register', [
+    AuthController::class, 'registerPost'
+  ])->name('register');
+  Route::get('/auth/login', [
+    AuthController::class, 'login'
+  ])->name('login');
+  Route::post('/auth/login', [
+    AuthController::class, 'loginPost'
+  ])->name('login');
 });
 Route::group(['middleware' => 'auth'], function () {
   Route::get('/dashboard', [
     DashboardController::class, 'index'
   ]);
+  
+  Route::get('/dashboard/forms', function () {
+    return view('dashboard.forms');
+  });
+  
+  Route::get('/dashboard/tables', function () {
+    return view('dashboard.tables');
+  });
+  
+  Route::get('/dashboard/ui-elements', function () {
+    return view('dashboard.ui-elements');
+  });
   
   Route::delete('/auth/logout', [
     AuthController::class, 'logout'
