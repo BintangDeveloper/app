ddf10ca61557c55d46f824a3db074b0da0ab762b
bintangdeveloper <bintangdeveloper@gmail.com>
2024-09-18 18:42:40 +0700

    +

--- resources/views/_layouts/header.blade.php
+++ resources/views/_layouts/header.blade.php
@@ -1,6 +1,6 @@
-<header class="flex items-center justify-between px-6 py-4 bg-black border-b-4 border-red-600">
+<header class="flex items-center justify-between px-6 py-4 bg-gray-800 {{-- border-b-4 border-red-600 --}}">
     <div class="flex items-center">
-        <button @click="sidebarOpen = true" class="text-gray-500 focus:outline-none lg:hidden">
+        <button @click="sidebarOpen = true" class=" text-red-600 {{-- text-gray-500 --}} focus:outline-none lg:hidden">
             <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M4 6H20M4 12H20M4 18H11" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
