de1cd50eee8cbe6e4ce5cf0d06662f3d02fb92be
bintangdeveloper <bintangdeveloper@gmail.com>
2024-09-18 18:09:12 +0700

    +

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,7 +2,10 @@
 
 @section('body')
     <h3 class="text-gray-700 text-3xl font-medium">Dashboard</h3>
+    
+    {{ Auth::user() }}
 
+    {{--
     <div class="mt-4">
         <div class="flex flex-wrap -mx-6">
             <div class="w-full px-6 sm:w-1/2 xl:w-1/3">
@@ -316,4 +319,5 @@
             </div>
         </div>
     </div>
+    --}}
 @endsection
