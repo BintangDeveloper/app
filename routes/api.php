5744411151bcf5104eae7e37376b0bbeba95c425
bintangdeveloper <bintangdeveloper@gmail.com>
2024-09-25 06:42:19 +0700

    +

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/up', function (Request $request) {
+  return response('Hello World', 200)->header('Content-Type', 'text/plain');
+});
