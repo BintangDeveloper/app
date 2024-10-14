788c39bb5539468720dc4901c7c15efd1ed26a3a
bintangdeveloper <bintangdeveloper@gmail.com>
2024-10-14 10:56:38 +0700

    +

--- config/cors.php
+++ config/cors.php
@@ -15,9 +15,17 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+      'api/*', 
+      'sanctum/csrf-cookie'
+    ],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => [
+      'GET', 
+      'POST', 
+      'PUT', 
+      'DELETE'
+      ],
 
     'allowed_origins' => [
       'https://bintangdeveloper.eu.org',
@@ -26,11 +34,14 @@ return [
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => [
+      'Content-Type', 
+      'Authorization'
+    ],
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => 3600,
 
     'supports_credentials' => true,
 
