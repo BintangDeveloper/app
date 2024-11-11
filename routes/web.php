ad7e869173f79a278d0cf791f469ce623ab7b5f7
bintangdeveloper <bintangdeveloper@gmail.com>
2024-11-11 11:14:10 +0700

    +

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
+use App\Helpers\JwtTokenHelper;
+
 Route::get('/', function (Request $request) {
     return view('welcome');
 //    return App\Helpers\ResponseHelper::error(
 //       "You don't have permission to access that or it is not allowed to access by anyone!", [
 //         $request
 //       ], 503);
+});
+
+Route::get('/_/gen', function (Request $request) {
+  return JwtTokenHelper::generateToken([
+    'permission' => 2
+  ], 86400);
 });
